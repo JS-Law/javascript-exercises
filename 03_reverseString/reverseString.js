bd7bd9920c0bbf97a5ffcb4e88bb109de538228c
Jeffrey Stanley <stanley.l.jeff@gmail.com>
2023-12-28 19:19:02 -0500

    Completed reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // if (string === "") return "ERROR"
+    let revString = ""
+    for (var i = string.length - 1; i >= 0; i--) {
+        revString += string[i];
+    }
+    return revString // Needed to RETURN the string!!!!!
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// PSEUDOCODE 
+// TAKE STRING
+// ITERATE BACKWARD FROM LAST LETTER AND ADD TO NEW STR
+// 
+// 
+// 
+// 
+// 
