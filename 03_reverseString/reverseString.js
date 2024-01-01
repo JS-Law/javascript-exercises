bc5bea5bcbc1d60938626a96ab25947fcd7e68dd
Jeffrey Stanley <stanley.l.jeff@gmail.com>
2024-01-01 18:20:24 -0500

    Completed remaining tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,3 +1,4 @@
+
 const reverseString = function(string) {
     // if (string === "") return "ERROR"
     let revString = ""
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,40 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+  
+    return newArray; // Add this line to return the modified array
+  };
+  
 
-};
+// const removeFromArray = function(arr, target) {
+//     let len = arr.length;
+//     let newArr = [];
+
+//     for (let i = 0; i < len; i++) {
+//         if (arr[i] !== target) {
+//             newArr.push(arr[i]);
+//         }
+//     }
+
+//     return newArr;
+// };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// WAS REMOVING BASED ON INDEX NOT VALUE
+// const removeFromArray = function(arr, target) {
+//     let len = arr.length
+//     let newArr = []
+//     for (let i = 0; i < len; i++) {
+//         if (target != i) {
+//             newArr.push(i);
+//             return newArr
+//         }
+//     }
+// };
