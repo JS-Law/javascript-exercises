f311618716587b12faf70f905b055c8bcdea6b30
Jeff Stanley <10554861+JS-Law@users.noreply.github.com>
2024-02-27 17:33:22 -0500

    Finish palindrome solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (x) {
+    let normalizedStr = x.toLowerCase().replace(/[^a-z0-9]/gi, '');
+    let strReversed = normalizedStr.split('').reverse().join('');
+    return normalizedStr === strReversed;
 };
 
 // Do not edit below this line
