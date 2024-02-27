801b0285a5caabb2cfdf87b0491aaa3c14402ebd
Jeff Stanley <10554861+JS-Law@users.noreply.github.com>
2024-02-27 17:58:45 -0500

    Complete fibonacci assignment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    const n = +input; // Convert input to a number
+    if (n < 0) return "OOPS";
+    let a = 0, b = 1, temp;
 
+    for (let i = 0; i < n; i++) {
+        temp = a;
+        a = b;
+        b = temp + b;
+    }
+
+    return a;
 };
 
-// Do not edit below this line
 module.exports = fibonacci;
+
+
+
